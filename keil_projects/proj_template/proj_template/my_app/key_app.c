3f416664cc5d1626e1538bec61d0c2f48e485785
hao <33110820@qq.com>
2023-11-29 20:06:42 +0800

    新增：按键代码，未完成

---
+++ keil_projects/proj_template/proj_template/my_app/key_app.c
@@ -0,0 +1,67 @@
+/**
+ * @file key_app.c
+ * @note 检测按键按下后开启定时器，每200ms检测一次按键是否还被按下，
+ *       到按键松开2s内无按键按下关闭定时器，2s内继续按下算连按，
+ *       能记录最后一次的按下时间，和总的按下次数，
+ *       检测到特殊事件后能回调用户提供的处理函数。
+ *
+ */
+#include "key_app.h"
+#include "mcu_hal.h"
+#include "stack_svc_api.h"
+
+#define KEY_PRESSED     TRUE     // 按下
+#define KEY_NOT_PRESSED FALSE    // 未按下
+
+KeyInfo_t keyInfo;
+static KeyCfg_t *pKeyCfg;
+
+static void key_scanTimerOff(void)
+{
+	// 查是否开启，如果开启则关闭
+	if (((ke_timer_active_handler)SVC_ke_timer_active)(APP_KEY_SCAN_TIMER, TASK_APP)) {
+		((ke_timer_clear_handler)SVC_ke_timer_clear)(APP_KEY_SCAN_TIMER, TASK_APP);
+	}
+}
+
+static void key_scanTimerOn(uint16 time)
+{
+	key_scanTimerOff();	// 防止之前开着定时器，先尝试关
+	((ke_timer_set_handler)SVC_ke_timer_set)(APP_KEY_SCAN_TIMER, TASK_APP, time);
+}
+
+static void key_scan(void)
+{
+	if (mcu_gpio_key_pressed()) {
+		keyInfo.keyStatus = KEY_PRESSED;
+	} else {
+		keyInfo.keyStatus = KEY_NOT_PRESSED;
+	}
+}
+
+void key_init(KeyCfg_t cfg)
+{
+	pKeyCfg = &cfg;
+}
+
+/**
+ * @brief 检测首次按下
+ * 
+ */
+void key_task(void)
+{
+	// 如果按键检测已开启，无需检测首次按下
+	if(keyInfo.scanStatus)
+		return;
+	key_scan();
+	if (keyInfo.keyStatus == KEY_PRESSED && !keyInfo.scanStatus) {
+		// 首次按下 开启扫描按键定时器
+		key_scanTimerOn(pKeyCfg->timerPeriod);
+		keyInfo.scanStatus = TRUE;
+	}
+}
+
+void key_scanTimerCB(void)
+{
+	;
+}
---
+++ keil_projects/proj_template/proj_template/my_app/key_app.h
@@ -0,0 +1,36 @@
+#ifndef __KEY_APP_H_
+#define __KEY_APP_H_
+
+#include "panble.h"
+
+typedef enum KeyEvents {
+	KEY_PRESS = 0,      // 检测到按键按下
+	KEY_PRESS_2SECS,    // 检测到按下2秒
+	KEY_PRESS_3SECS,    // 检测到按下3秒
+	KEY_RELEASE,        // 检测到按键释放
+	KEY_SCAN_END,       // 按键检测结束（无后续连按）
+} KeyEvents_t;
+
+typedef void (*KeyEventFuncPtr)(KeyEvents_t, uint8);    // 无返回 输入事件与连按次数
+
+typedef struct KeyInfo {
+	uint8 scanStatus;      // 0:未扫描 1:正在扫描
+	uint8 keyStatus;       // 0:未按下 1:按下
+	uint8 pressCnt;        // 连按次数
+	uint16 pressTime;      // 最后一次按下时长(ms)
+	uint16 releaseTime;    // 松开时长(ms)
+} KeyInfo_t;
+
+typedef struct KeyCfg {
+	uint8 timerPeriod;             // 定时器周期、检测周期(单位10ms)
+	uint8 pressIntervalTime;       // 连按最大间隔时间(单位10ms 需为检测周期整数倍)
+	KeyEventFuncPtr keyEventCB;    // 按键事件回调函数
+} KeyCfg_t;
+
+extern KeyInfo_t keyInfo;
+
+void key_init(KeyCfg_t cfg);
+void key_task(void);
+void key_scanTimerCB(void);
+
+#endif    //__KEY_APP_H_
